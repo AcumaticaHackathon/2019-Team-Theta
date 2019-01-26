5a0899ce9c8aa0290892fb5f78f972d7d719db57
Jeremy Osterhouse <jeremy@onetencommunications.com>
2019-01-26 18:56:47 -0500

    Add GetLists to Mailchimp manager

---
+++ MailchimpPX/MailchimpPXManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MailChimp.Net;
+using MailChimp.Net.Core;
+      
+namespace MailchimpPX
+{
+    public class MailchimpPXManager
+    {
+        private readonly MailChimp.Net.MailChimpManager manager;
+
+        public MailchimpPXManager(string apiKey)
+        {
+            manager = new MailChimpManager(apiKey);
+        }
+
+        public List<MCList> GetLists()
+        {
+            var mc_lists = Task.Run(async () => await manager.Lists.GetAllAsync()).Result;
+            var result = new List<MCList>();
+            foreach (var item in mc_lists)
+            {
+                result.Add(new MCList { ID = item.Id, Name = item.Name });
+            }
+            return result;
+        }
+
+        public void AddEmailToList(string email, string listID, string first_name, string last_name, )
+        {
+            
+        }
+
+
+    }
+}
---
+++ MailchimpPX/models/MCList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailchimpPX
+{
+    public class MCList
+    {
+        public string ID { set; get; }
+        public string Name { set; get; }
+    }
+}
